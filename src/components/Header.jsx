d390d84999f891dbc1ed58508d634ba8cb272da1
Kenneth Leeroy <misakenleeroy@gmail.com>
2022-11-16 12:49:56 +0400

    Added the spinner component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 
+
 const Header = () => {
   const [pageState, setPageState] = useState('Sign In')
   const navigate = useNavigate();
@@ -30,7 +31,7 @@ const Header = () => {
     <div className='bg-white border-b shadow-sm sticky top-0 z-40'>
         <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
             <div>
-                <img src="https://www.freepnglogos.com/uploads/logo-home-png/rumah-dijual-dijual-cepat-rumah-jembatan-11.png" alt="logo" className='h-10 cursor-pointer'
+                <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo" className='h-10 cursor-pointer'
                 onClick={()=>{
                     navigate('/')
                 }}/>
