2e3ac3741cee5809002765c02ed4544579e92dc1
Kenneth Leeroy <misakenleeroy@gmail.com>
2022-11-16 14:07:48 +0400

    Created the Listing Page UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
     <div className='bg-white border-b shadow-sm sticky top-0 z-40'>
         <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
             <div>
-                <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo" className='h-10 cursor-pointer'
+                <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo" className='h-5 cursor-pointer'
                 onClick={()=>{
                     navigate('/')
                 }}/>
